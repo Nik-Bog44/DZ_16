984c84c8cfea652d64966a5b3b2ba51c8f743230
Nik-Bog44 <dhjan44@gmail.com>
2023-08-01 00:11:29 +0300

    done

--- app/utils.py
+++ app/utils.py
@@ -1,12 +1,37 @@
+import json
+
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
 from models import db, User, Order, Offer
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///memory:'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['RESTS_JSON'] = {'unsure_ascii': False, 'indent': 4}
 
-db.init_app(app)
+
+db = SQLAlchemy(app)
+
+
+def load_data_from_json(json_file, model_class):
+    with open(json_file, 'r') as file:
+        data = json.load(file)
+        for item in data:
+            new_entry = model_class(**item)
+            db.session.add(new_entry)
+    db.session.commit()
+    # Создание таблиц в базе данных
+    db.create_all()
+    # Путь к файлам JSON в директории Data
+    users_file = 'Data/users.json'
+    orders_file = 'Data/orders.json'
+    offers_file = 'Data/offers.json'
+
+    # Загрузка данных из JSON файлов в соответствующие таблицы
+    load_data_from_json(users_file, User)
+    load_data_from_json(orders_file, Order)
+    load_data_from_json(offers_file, Offer)
 
 
 # Маршрут для получения всех пользователей
@@ -56,101 +81,111 @@ def get_user_by_id(user_id):
     return jsonify(user_data)
 
     # PUT-запрос для обновления пользователя по идентификатору
+
+
 @app.route('/users/<int:user_id>', methods=['PUT'])
 def update_user(user_id):
-        user = User.query.get(user_id)
-        if not user:
-            return jsonify({'message': 'User not found'}), 404
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({'message': 'User not found'}), 404
 
-        data = request.json  # Получаем данные из тела запроса (JSON)
-        user.first_name = data['first_name']
-        user.last_name = data['last_name']
-        user.age = data['age']
-        user.email = data['email']
-        user.role = data['role']
-        user.phone = data['phone']
+    data = request.json  # Получаем данные из тела запроса (JSON)
+    user.first_name = data['first_name']
+    user.last_name = data['last_name']
+    user.age = data['age']
+    user.email = data['email']
+    user.role = data['role']
+    user.phone = data['phone']
 
-        db.session.commit()
-        return jsonify({'message': 'User updated successfully'}), 200
+    db.session.commit()
+    return jsonify({'message': 'User updated successfully'}), 200
 
-    # DELETE-запрос для удаления пользователя по идентификатору
+
+# DELETE-запрос для удаления пользователя по идентификатору
 @app.route('/users/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
-        user = User.query.get(user_id)
-        if not user:
-            return jsonify({'message': 'User not found'}), 404
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({'message': 'User not found'}), 404
 
-        db.session.delete(user)
-        db.session.commit()
-        return jsonify({'message': 'User deleted successfully'}), 200
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({'message': 'User deleted successfully'}), 200
 
-    # GET-запрос для получения всех заказов
+
+# GET-запрос для получения всех заказов
 @app.route('/orders', methods=['GET'])
 def get_all_orders():
-        orders = Order.query.all()
-        return jsonify([order.serialize_order() for order in orders]), 200
+    orders = Order.query.all()
+    return jsonify([order.serialize_order() for order in orders]), 200
 
-    # GET-запрос для получения заказа по идентификатору
+
+# GET-запрос для получения заказа по идентификатору
 @app.route('/orders/<int:order_id>', methods=['GET'])
 def get_order_by_id(order_id):
-        order = Order.query.get(order_id)
-        if not order:
-            return jsonify({'message': 'Order not found'}), 404
-        return jsonify(order.serialize_order()), 200
+    order = Order.query.get(order_id)
+    if not order:
+        return jsonify({'message': 'Order not found'}), 404
+    return jsonify(order.serialize_order()), 200
+
 
 @app.route('/orders', methods=['POST'])
 def create_order():
-        data = request.json
-        if not data:
-            return jsonify({'message': 'No data provided'}), 400
+    data = request.json
+    if not data:
+        return jsonify({'message': 'No data provided'}), 400
 
-        order = Order()
-        order.update_fr
+    order = Order()
+    order.update_fr
 
-        db.session.add(order)
-        db.session.commit()
+    db.session.add(order)
+    db.session.commit()
 
-        return jsonify(order.serialize()), 201
+    return jsonify(order.serialize()), 201
+
 
 @app.route('/orders/<int:order_id>', methods=['PUT'])
 def update_order(order_id):
-        order = Order.query.get(order_id)
-        if not order:
-            return jsonify({'message': 'Order not found'}), 404
+    order = Order.query.get(order_id)
+    if not order:
+        return jsonify({'message': 'Order not found'}), 404
 
-        data = request.json
-        if not data:
-            return jsonify({'message': 'No data provided'}), 400
+    data = request.json
+    if not data:
+        return jsonify({'message': 'No data provided'}), 400
 
-        order.update_from_json(data)
-        db.session.commit()
+    order.update_from_json(data)
+    db.session.commit()
 
-        return jsonify(order.serialize_order()), 200
+    return jsonify(order.serialize_order()), 200
+
 
 @app.route('/orders/<int:order_id>', methods=['DELETE'])
 def delete_order(order_id):
-        order = Order.query.get(order_id)
-        if not order:
-            return jsonify({'message': 'Order not found'}), 404
+    order = Order.query.get(order_id)
+    if not order:
+        return jsonify({'message': 'Order not found'}), 404
 
-        db.session.delete(order)
-        db.session.commit()
+    db.session.delete(order)
+    db.session.commit()
 
-        return jsonify({'message': 'Order deleted successfully'}), 200
+    return jsonify({'message': 'Order deleted successfully'}), 200
 
-    # GET-запрос для получения всех предложений
+
+# GET-запрос для получения всех предложений
 @app.route('/offers', methods=['GET'])
 def get_all_offers():
-        offers = Offer.query.all()
-        return jsonify([offer.serialize_offer() for offer in offers]), 200
+    offers = Offer.query.all()
+    return jsonify([offer.serialize_offer() for offer in offers]), 200
 
-    # GET-запрос для получения предложения по идентификатору
+
+# GET-запрос для получения предложения по идентификатору
 @app.route('/offers/<int:offer_id>', methods=['GET'])
 def get_offer_by_id(offer_id):
-        offer = Offer.query.get(offer_id)
-        if not offer:
-            return jsonify({'message': 'Offer not found'}), 404
-        return jsonify(offer.serialize_offer()), 200
+    offer = Offer.query.get(offer_id)
+    if not offer:
+        return jsonify({'message': 'Offer not found'}), 404
+    return jsonify(offer.serialize_offer()), 200
 
 
 # Вспомогательная функция для обновления атрибутов заказа из JSON
@@ -222,5 +257,3 @@ def delete_offer(offer_id):
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
